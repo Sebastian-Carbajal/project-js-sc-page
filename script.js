b99fe8738403bb5e6f8e5c191db95d37b795bf4b
Sebastian-Carbajal <scarbajal283@gmail.com>
2022-11-14 22:46:46 -0300

    Ultima modificacion a la app web

--- script.js
+++ script.js
@@ -1,34 +1,12 @@
 // Aplicación Web basado en un convertidor de divisas
 
-// Variables globales
-
-    // Acumulador
-    let resultado = 0
-
-// Variables con valor de las conversiones actual
-
-let arsToUsd = 0.006
-let arsToEur = 0.006
-let arsToGbp = 0.005
-
-let eurToArs = 155.4
-let eurToGbp = 0.85
-let eurToUsd = 0.99
-
-let usdToArs = 156.1
-let usdToGbp = 0.86
-let usdToEur = 1.00
-
-let gbpToArs = 181.1
-let gbpToUsd = 1.16
-let gbpToEur = 1.16
+// Variables globales -- Acumulador 
+let resultado = 0
 
 // Lista de divisas disponibles para poder ingresar
-
-const divisasDisponibles = ["Pesos argentinos","Dolares","Libras esterlinas","Euros"]
+const divisasDisponibles = ["Pesos argentinos","Dolares","Libras esterlinas","Euros","Soles","Yenes","Francos"]
 
 // Método para ordenar la lista de divisas en orden alfabético
-
 const ordenadoDeDivisas = divisasDisponibles.sort ( (a,b) => {
     if(a > b){
         return 1
@@ -38,28 +16,45 @@ const ordenadoDeDivisas = divisasDisponibles.sort ( (a,b) => {
         return 0
     }
 })
-// console.log(ordenadoDeDivisas)
 
+// Llamada y creación de elementos 
 
-// Llamada de elementos del DOM
-// Creando un contenedor
-
+// Llamada al contenedor
 const contenedor = document.getElementById('container')
 
-// Creando un texto para indicar donde ingresar la cantidad a convertir
+// Llamada a la caja
+const caja = document.getElementById('cajaConversion')
+caja.className = 'caja'
 
-const quantityText = document.createElement('p')
-contenedor.append(quantityText)
-quantityText.innerText = 'Cantidad:'
+// Creación y llamada de elementos que serán el contenido de la caja
+const palabra = document.getElementById('parrafoDeCantidad')
+palabra.style.fontWeight = 'bold'
 
-// Creando un campo de texto de entrada y añadiéndolo al contenedor
+const quantityInput = document.getElementById('numberDeEntrada')
+quantityInput.className = 'padding'
 
-const quantityInput = document.createElement("input")
-contenedor.append(quantityInput)
+const convertirDe = document.createElement('p')
 
-// Creación de un menú desplegable
+// Adición del contenido de texto que acompaña al primer select
+convertirDe.innerText = 'Convertir de:'
+caja.append(convertirDe)
+convertirDe.style.fontSize = '1.2rem'
 
-const fluentSelect = document.createElement("select")
+// Llamada al primer select
+const fluentSelect = document.getElementById('firstSelect')
+caja.append(fluentSelect)
+fluentSelect.className = 'editSelects'
+
+// Adición del contenido de texto que acompaña al segundo select
+const string = document.createElement("p")
+string.innerText = 'a:'
+caja.append(string)
+string.style.fontSize = '1.2rem'
+
+// Llamada al segundo select 
+const secondSelect = document.getElementById('secondSelect')
+caja.append(secondSelect)
+secondSelect.className = 'editSelects'
 
 // Agregando los elementos del array divisasDisponibles como las opciones del menú
 // Añadiendo las opciones al menú
@@ -71,24 +66,7 @@ for (div of divisasDisponibles) {
     fluentSelect.append(options)
 }
 
-// Añadiendo el menú al contenedor
-
-contenedor.append(fluentSelect)
-
-// Evento en que aparece la opción seleccionada en consola
-
-contenedor.addEventListener("change", () => {
-    let selectDinamico = fluentSelect.value
-    // console.log(selectDinamico)
-})
-
-console.log(contenedor)
-
-// Creación del otro menú desplegable
-
-const secondSelect = document.createElement("select")
-
-// Agregando los elementos del array list como las opciones del menú
+// Agregando los elementos del array divisasDisponibles como las opciones del menú
 // Añadiendo las opciones al menú
 
 for (div of divisasDisponibles) {
@@ -98,171 +76,185 @@ for (div of divisasDisponibles) {
     secondSelect.append(opciones)
 }
 
-// Añadiendo el otro menú al contenedor
-
-contenedor.append(secondSelect)
-
-// Creación de botón
+// Creación de botón de conversión
 const liveButton = document.createElement("button")
 liveButton.innerHTML = "CONVERTIR"
-contenedor.append(liveButton)
+caja.append(liveButton)
+liveButton.className = 'padding'
 
 
-// Creando texto para el output del resultado de la conversión
+// Creación del texto que indica un mensaje respecto al resultado de la conversión 
 const textoFinal = document.createElement('p')
 textoFinal.innerText = 'El resultado de la conversión es de:'
-contenedor.append(textoFinal)
+caja.append(textoFinal)
+textoFinal.style.fontWeight = 'bold'
 
-// Creando un campo de texto de output de resultado de conversión
+// Creación del campo de texto que será el output de resultado de la conversión
 const outputResult = document.createElement('input')
-contenedor.append(outputResult)
+caja.append(outputResult)
+outputResult.className = 'padding'
 
-// Creando texto para devolver divisa convertida en la página
+// Creación del texto que devuelve divisa convertida en la página
 const textoFinal2 = document.createElement('p')
-contenedor.append(textoFinal2)
+caja.append(textoFinal2)
 
-// Función para solamente escribir valores mayores a 0 ********
-// Function o condición en caso de ingresar un número negativo o cero en la cantidad a convertir ******
+// Creación del botón de reversa para revertir la conversión 
+const reverseButton = document.createElement('button')
+reverseButton.innerHTML = "<i class='bx bx-revision'></i>"
+caja.append(reverseButton)
+reverseButton.className = 'padding'
 
-// quantityInput.addEventListener('change',validarCantidad)
-
-function validarCantidad () {
-    if(quantityInput.value < 0) {
-        resultado = 0
-    } else if (quantityInput.value === 0) {
-        resultado = 0
-    }
-}
-
-// Función para el caso de que se seleccionen los mismos valores en ambos selects
-
-function valoresIguales() {
-    if((fluentSelect.value === secondSelect.value) && (quantityInput.value < 0)){
-        resultado = 0
-    } else if ((fluentSelect.value === secondSelect.value)) {
-        resultado = quantityInput.value
-    }
-}   
-
-
-// Función para hacer los cálculos de conversión
-
-function calcularConversion () {
-    resultado = 0
-
-    if(fluentSelect.value === 'Dolares') {
-        switch(secondSelect.value){
-            case 'Euros':
-                resultado = quantityInput.value * usdToEur
-                break
-            case 'Libras esterlinas':
-                resultado = quantityInput.value * usdToGbp
-                break
-            case 'Pesos argentinos':
-                resultado = quantityInput.value * usdToArs
-                break
-            default:
-                resultado = NaN
-        }
-    }
-    else if(fluentSelect.value === 'Euros'){
-        switch(secondSelect.value){
-            case 'Pesos argentinos':
-                resultado = quantityInput.value * eurToArs
-                break
-            case 'Libras esterlinas':
-                resultado = quantityInput.value * eurToGbp
-                break
-            case 'Dolares':
-                resultado = quantityInput.value * eurToUsd
-                break
-            default:
-                resultado = NaN
-
-        }
-    }
-    else if(fluentSelect.value === 'Libras esterlinas'){
-        switch(secondSelect.value){
-            case 'Pesos argentinos':
-                resultado = quantityInput.value * gbpToArs
-                break
-            case 'Dolares':
-                resultado = quantityInput.value * gbpToUsd
-                break
-            case 'Euros':
-                resultado = quantityInput.value * gbpToEur
-                break
-            default:
-                resultado = NaN
-
-        }
-    }
-    else if(fluentSelect.value === 'Pesos argentinos'){
-        switch(secondSelect.value){
-            case 'Dolares':
-                resultado = quantityInput.value * arsToUsd
-                break
-            case 'Euros':
-                resultado = quantityInput.value * arsToEur
-                break
-            case 'Libras esterlinas':
-                resultado = quantityInput.value * arsToGbp
-                break
-            default:
-                resultado = NaN
-        }
-    }
-    else {
-        resultado = NaN
-    }
-
-    
-    valoresIguales()
-    outputResult.value = resultado
-    textoFinal2.innerHTML = secondSelect.value
-    
-    // Pasaje del resultado de la conversión (tipo number) a JSON  
-    JSON.stringify(localStorage.setItem('Resultado_De_Conversion',resultado))
-    const resultadoConversion = JSON.stringify(resultado)
-    // Resultado de la conversión mostrado en consola como tipo string (pasado a JSON)
-    // console.log(typeof resultadoConversion)
-    validarCantidad()
-}
 
 // Evento al presionar el botón
 liveButton.addEventListener("click",calcularConversion)
-liveButton.addEventListener("click", () => {
 
-    // Pasaje de la cantidad (tipo number) a JSON 
-    JSON.stringify(localStorage.setItem('Cantidad',quantityInput.value))
-    const cantidadAConvertir = JSON.stringify(quantityInput.value)
-    // Cantidad a convertir mostrada en consola como tipo string (pasada a JSON) 
-    // console.log(typeof cantidadAConvertir)
-    
-    // Pasaje de la cantidad de tipo JSON (string) a tipo number
-    const obtenerCantidad = localStorage.getItem('Cantidad')
-    const cantidadInput = JSON.parse(obtenerCantidad)
-    // Cantidad pasada de JSON a su tipo de objeto number 
-    // console.log(typeof cantidadInput)
+// Evento para el caso de que se ingrese una cantidad negativa
+liveButton.addEventListener("click",cantidadNegativa)
 
-    // Pasaje del resultado de la conversión de JSON (tipo string) a tipo number
-    const obtenerResultado = localStorage.getItem('Resultado_De_Conversion')
-    const resultadoInput = JSON.parse(obtenerResultado)
-    // Resultado de la conversión pasada de JSON a su tipo de objeto number
-    // console.log(typeof resultadoInput)
+// Evento para el botón de revertir
+reverseButton.addEventListener('click', () => {
 
-    // Pasaje de la primera divisa a JSON 
-    JSON.stringify(localStorage.setItem('Divisa_A_Convertir',fluentSelect.value))
-    JSON.stringify(fluentSelect.value)
-
-    // Pasaje de la segunda divisa a JSON 
-    JSON.stringify(localStorage.setItem('Divisa_A_La_Que_Fue_Convertida',secondSelect.value)) 
-    JSON.stringify(secondSelect.value)
-
-    // Se convoca a la divisa que fue convertida a la consola 
-    const divisaDos = localStorage.getItem('Divisa_A_La_Que_Fue_Convertida')
-    // console.log(divisaDos)
+    const tempSelect = fluentSelect.value
+    fluentSelect.value = secondSelect.value
+    secondSelect.value = tempSelect
+    calcularConversion()
+    cantidadNegativa()
 })
+
+// Evento para la activación/desactivación del botón de revertir 
+reverseButton.addEventListener('click', () => {
+    deshabilitarBoton()
+    setTimeout( () => {
+        deshabilitarBoton(false)}, 400)
+})
+
+
+
+// Función para hacer los cálculos de conversión
+function calcularConversion () {
+    const primerSelect = fluentSelect.value
+    const segundoSelect = secondSelect.value
+    const cantAConvertir = quantityInput.value
+        fetch(`json/divisas.json`)
+        .then((resp) => {
+            return resp.json()
+        }
+        )
+        .then( (divisas) => {
+
+        const dolar = primerSelect === 'Dolares'&& true
+        const dolar2 = segundoSelect === 'Dolares' && true
+        const euro = primerSelect === 'Euros' && true
+        const euro2 = segundoSelect === 'Euros' && true
+        const libra = primerSelect === 'Libras esterlinas' && true
+        const libra2 = segundoSelect === 'Libras esterlinas' && true
+        const peso = primerSelect === "Pesos argentinos" && true
+        const peso2 = segundoSelect === 'Pesos argentinos' && true
+        const sol = primerSelect === 'Soles'&& true
+        const sol2 = segundoSelect === 'Soles' && true
+        const yen = primerSelect === 'Yenes' && true
+        const yen2 = segundoSelect === 'Yenes' && true
+        const franco = primerSelect === 'Francos' && true
+        const franco2 = segundoSelect === 'Francos' && true
+        
+
+divisas.forEach(div => {
+
+    setTimeout ( () => {             
+            if(div.divisa === 'Pesos argentinos' && peso){
+                const resultado = (peso && dolar2) ? (cantAConvertir * div.conversionesArs.arsToUsd) : (peso && libra2) ? (cantAConvertir * div.conversionesArs.arsToGbp) : (peso && euro2) ? (cantAConvertir * div.conversionesArs.arsToEur) : (peso && sol2) ? (cantAConvertir * div.conversionesArs.arsToPen) : (peso && yen2) ? (cantAConvertir * div.conversionesArs.arsToJpy) : (peso && franco2) ? (cantAConvertir * div.conversionesArs.arsToChf) : cantAConvertir
+                outputResult.value = resultado
+            } else if (div.divisa === 'Dolares' && dolar){
+                const resultado2 = (dolar && peso2) ? (cantAConvertir * div.conversionesUsd.usdToArs) : (dolar && libra) ? (cantAConvertir * div.conversionesUsd.usdToGbp) : (dolar && euro2) ? (cantAConvertir * div.conversionesUsd.usdToEur) : (dolar && sol2) ? (cantAConvertir * div.conversionesUsd.usdToPen) : (dolar && yen2) ? (cantAConvertir * div.conversionesUsd.usdToJpy) : (dolar && franco2) ? (cantAConvertir * div.conversionesUsd.usdToChf) : cantAConvertir
+                outputResult.value = resultado2
+            } else if (div.divisa === 'Libras esterlinas' && libra) {
+                const resultado3 = (libra && peso2) ? (cantAConvertir * div.conversionesGbp.gbpToArs) : (libra && dolar2) ? (cantAConvertir * div.conversionesGbp.gbpToUsd) : (libra && euro2) ? (cantAConvertir * div.conversionesGbp.gbpToEur) : (libra && sol2) ? (cantAConvertir * div.conversionesGbp.gbpToPen) : (libra && yen2) ? (cantAConvertir * div.conversionesGbp.gbpToJpy) : (libra && franco2) ? (cantAConvertir * div.conversionesGbp.gbpToChf) : cantAConvertir
+                outputResult.value = resultado3
+            } else if (div.divisa === 'Euros' && euro) {
+                const resultado4 = (euro && peso2) ? (cantAConvertir * div.conversionesEur.eurToArs) : (euro && dolar2) ? (cantAConvertir * div.conversionesEur.eurToUsd) : (euro && libra2) ? (cantAConvertir * div.conversionesEur.eurToGbp) : (euro && sol2) ? (cantAConvertir * div.conversionesEur.eurToPen) : (euro && yen2) ? (cantAConvertir * div.conversionesEur.eurToJpy) : (euro && franco2) ? (cantAConvertir * div.conversionesEur.eurToChf) : cantAConvertir 
+                outputResult.value = resultado4
+            } else if (div.divisa === 'Soles peruanos' && sol) {
+                const resultado5 = (sol && peso2) ? (cantAConvertir * div.conversionesPen.penToArs) : (sol && dolar2) ? (cantAConvertir * div.conversionesPen.penToUsd) : (sol && libra2) ? (cantAConvertir * div.conversionesPen.penToGbp) : (sol && euro2) ? (cantAConvertir * div.conversionesPen.penToEur) : (sol && yen2) ? (cantAConvertir * div.conversionesPen.penToJpy) : (sol && franco2) ? (cantAConvertir * div.conversionesPen.penToChf) : cantAConvertir
+                outputResult.value = resultado5
+            } else if (div.divisa === 'Yenes' && yen) {
+                const resultado6 = (yen && peso2) ? (cantAConvertir * div.conversionesJpy.jpyToArs) : (yen && dolar2) ? (cantAConvertir * div.conversionesJpy.jpyToUsd) : (yen && libra2) ? (cantAConvertir * div.conversionesJpy.jpyToGbp) : (yen && euro2) ? (cantAConvertir * div.conversionesJpy.jpyToEur) : (yen && sol2) ? (cantAConvertir * div.conversionesJpy.jpyToPen) : (yen && franco2) ? (cantAConvertir * div.conversionesJpy.jpyToChf) : cantAConvertir
+                outputResult.value = resultado6
+            } else if (div.divisa === 'Francos suizos' && franco) {
+                const resultado7 = (franco && peso2) ? (cantAConvertir * div.conversionesChf.chfToArs) : (franco && dolar2) ? (cantAConvertir * div.conversionesChf.chfToUsd) : (franco && libra2) ? (cantAConvertir * div.conversionesChf.chfToGbp) : (franco && euro2) ? (cantAConvertir * div.conversionesChf.chfToEur) : (franco && sol2) ? (cantAConvertir * div.conversionesChf.chfToPen) : (franco && yen2) ? (cantAConvertir * div.conversionesChf.chfToJpy) : cantAConvertir
+                outputResult.value = resultado7 
+            }
+            
+            textoFinal2.style.fontWeight = '800'
+            textoFinal2.innerHTML = segundoSelect},400)
+        })
+})
+        valoresIguales()
+        cantidadIgualaCero()
+
+    // Pasaje de los valores al LocalStorage 
+
+    JSON.stringify(localStorage.setItem('Cantidad_A_Convertir',cantAConvertir))
+    JSON.stringify(localStorage.setItem('Divisa_De_Entrada',primerSelect))
+    JSON.stringify(localStorage.setItem('Divisa_De_Salida',segundoSelect))
+    JSON.stringify(localStorage.setItem('Resultado_De_Conversion',outputResult.value))
+
+}
+
+// Función que se añade al evento del caso de ingreso de un valor negativo
+function cantidadNegativa () {
+    return new Promise( (resolve,reject) => {
+        if(quantityInput.value < 0) {
+                resolve(
+                Swal.fire ({
+                    icon: 'error',
+                    title: 'ERROR',
+                    text: 'No se pueden convertir valores negativos!',
+                    width: 600,
+                    padding: 50,
+                }).then((action) => {
+                    if(action.isConfirmed) {
+                        outputResult.value = 0
+                        quantityInput.value = ''
+                        calcularConversion()
+                    } else if (action.isDismissed) {
+                        outputResult.value = 0
+                        quantityInput.value = ''
+                        calcularConversion()
+                    }
+                }))
+        }
+    })
+}
+
+// Función para el caso de que se seleccionen los mismos valores en ambos selects
+function valoresIguales() {
+    if(fluentSelect.value === secondSelect.value) {
+        outputResult.value = quantityInput.value
+    }
+}
+// Funcion para el caso de que la cantidad ingresada sea cero
+function cantidadIgualaCero () {
+    if(quantityInput.value === 0) {
+        outputResult.value = 0
+    }
+}
+
+// Función para bloquear el botón de revertir evitando su accionar reiteradas veces   
+function deshabilitarBoton (boton = true) {
+    if (boton) {
+        reverseButton.disabled = true
+    } else {
+        reverseButton.disabled = ''
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
